e242a786fe2d5096d4778e72eb60581708ae7584
Dominic Guinane <dominic.guinane@7digital.com>
2017-06-29 17:32:47 +0100

    Revert "Unset IFS after loop"
    This reverts commit 5ec4065b335765345b35539c3eaa393c656ea6a2.

--- include/wrapper.sh
+++ include/wrapper.sh
@@ -31,7 +31,6 @@ for zk in "${zookeepers[@]}"; do
     ZOO_CFG_EXTRA=$ZOO_CFG_EXTRA"&server."$COUNTER"\="$zk;
     COUNTER=`expr $COUNTER + 1`
 done
-unset IFS
 
 cat <<- EOF > /opt/exhibitor/defaults.conf
 	zookeeper-data-directory=$ZK_DATA_DIR
